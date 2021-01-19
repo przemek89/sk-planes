3d292dccd45750b79c4ce9c15b0ec14564a58057
Przemek Żbikowski <you@example.com>
2021-01-19 14:37:41 +0100

    - added Flights module - created header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { DashboardComponent } from "./core/dashboard/dashboard.component";
 import { LoginComponent } from "./core/login/login.component";
+import { FlightsComponent } from "./flights/flights.component";
 
 const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full'},
-    { path: 'login', component: LoginComponent},
-    { path: 'dashboard', component: DashboardComponent}
+    { path: 'login', component: LoginComponent },
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+        children: [
+            { path: '', redirectTo: 'flights', pathMatch: 'full'},
+            { path: 'flights', component: FlightsComponent}
+        ]
+    }
 ]
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../material/material.module';
 
 
 
 @NgModule({
   declarations: [DashboardComponent, LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    RouterModule
   ],
   exports: [
     DashboardComponent, LoginComponent
